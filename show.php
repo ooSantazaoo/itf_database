71891a7261e295bce20ffe3543ae9fe5bd585245
ooSantazaoo <73683767+ooSantazaoo@users.noreply.github.com>
2020-11-17 22:10:30 +0700

    Update show.php

--- show.php
+++ show.php
@@ -1,6 +1,11 @@
 <html>
 <head>
 <title>ITF Lab</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+      integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
 <body>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
