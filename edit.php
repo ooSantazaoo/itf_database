c2bdb9053fead4ce9f7069b875f2ac061da229fb
ooSantazaoo <73683767+ooSantazaoo@users.noreply.github.com>
2020-11-17 23:11:49 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -6,11 +6,9 @@ $res = mysqli_query($conn, "SELECT * FROM guestbook WHERE ID='$id'");
 $row = mysqli_fetch_array($res)
 ?>
 <form action = "update.php?ID=<?php echo $row['ID']; ?>" method = "post" id="CommentForm">
-    Name:<br>
-    <input type="text" name = "name" id="idName" value="<?php echo "$row[name]"; ?>" <br>
-    Comment:<br>
-    <input type="text" name = "comment" id="idComment" value="<?php echo "$row[comment]"; ?>" <br>
-    Link:<br>
-    <input type="text" name = "link" id="idLink" value="<?php echo "$row[link]"; ?>"> <br><br>
+    
+    <input type="text" name = "name" id="idName" value="<?php echo "$row[name]"; ?>" Name:<br>
+    <input type="text" name = "comment" id="idComment" value="<?php echo "$row[comment]"; ?>" Comment:<br>
+    <input type="text" name = "link" id="idLink" value="<?php echo "$row[link]"; ?>"> <br><br>Link:<br>
     <input type="submit" id="commentBtn"class="btn btn-outline-warning">
   </form>
