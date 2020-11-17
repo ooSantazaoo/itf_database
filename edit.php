e38de1278fbd4663953e02b1474b62358a90f330
ooSantazaoo <73683767+ooSantazaoo@users.noreply.github.com>
2020-11-17 21:53:29 +0700

    Add files via upload

---
+++ edit.php
@@ -0,0 +1,16 @@
+<?php
+$id=$_GET['ID'];
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'santanakano.mysql.database.azure.com', 'ooSantazaoo@santanakano', 'santa+123', 'itf_lab', 3306);
+$res = mysqli_query($conn, "SELECT * FROM guestbook WHERE ID='$id'");
+$row = mysqli_fetch_array($res)
+?>
+<form action = "update.php?ID=<?php echo $row['ID']; ?>" method = "post" id="CommentForm">
+    Name:<br>
+    <input type="text" name = "Name" id="idName" value="<?php echo "$row[name]"; ?>" <br>
+    Comment:<br>
+    <input type="text" name = "Comment" id="idComment" value="<?php echo "$row[comment]"; ?>" <br>
+    Link:<br>
+    <input type="text" name = "Link" id="idLink" value="<?php echo "$row[link]"; ?>"> <br><br>
+    <input type="submit" id="commentBtn"class="btn btn-outline-warning">
+  </form>
